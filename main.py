48887cbbc02c339cc2da8520edfc1c70d475baad
lu656 <lu656@purdue.edu>
2021-04-27 12:57:15 -0400

    getting ready for training

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import os
-from pycocotools.coco import COCO
-import numpy as np
-import skimage.io as io
+import sys
 import matplotlib.pyplot as plt
-import torch
-import torch.utils.data as data
+import numpy as np
 from PIL import Image
+from collections import Counter
+from tqdm import tqdm
+import time
+
+from pycocotools.coco import COCO
+
+import nltk
+
+import skimage.io as io
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+from torch.autograd import Variable
 from torchvision import transforms
+
+from vocab import Vocab
+from model import EncoderCNN, DecoderRNN
+from utils import train, validate, save_epoch, early_stopping
  
- 
+
+###############################################################################
+###############################################################################
+# PREPROCESSING
+###############################################################################
+###############################################################################
+
 # Load MS COCO data, and use API for annotations and caption
 train_coco = COCO('./cocoapi/annotations/instances_train2014.json')
-train_coco_caps = COCO('./cocoapi/annotations/captions_train2014.json')
- 
-# # all images ids
-ids = list(train_coco.anns.keys())
+val_coco = COCO('./cocoapi/annotations/instances_val2014.json')
 
+# file path to the actual images
 train_file_path = './cocoapi/images/train2014'
 val_file_path = './cocoapi/images/val2014'
 test_file_path = './cocoapi/images/test2014'
@@ -23,43 +41,70 @@ test_file_path = './cocoapi/images/test2014'
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
-# Define a transform to pre-process the training images.
-# transform_train = transforms.Compose([ 
-#     transforms.Resize(256),                          # smaller edge of image resized to 256
-#     transforms.RandomCrop(224),                      # get 224x224 crop from random location
-#     transforms.RandomHorizontalFlip(),               # horizontally flip image with probability=0.5
-#     transforms.ToTensor(),                           # convert the PIL Image to a tensor
-#     transforms.Normalize((0.485, 0.456, 0.406),      # normalize image for pre-trained model
-                        #  (0.229, 0.224, 0.225))])
-
-def load_captions(image_id):
-    annIds = train_coco_caps.getAnnIds(imgIds=image_id)
-    anns = train_coco_caps.loadAnns(annIds)
-    return train_coco_caps.showAnns(anns)
-    
+def load_captions(image_id, coco_caps):
+    annIds = coco_caps.getAnnIds(imgIds=image_id)
+    anns = coco_caps.loadAnns(annIds)
+    return anns
 
 class transform_dataset(data.Dataset):
-    # def __init__(self, images):
-    #     self.img = images
-    def __init__(self, indices):
-        self.index = indices
+
+    def __init__(self, coco, annotations_file, image_folder, batch_size):
+        self.coco = coco
+        self.index = list(self.coco.anns.keys())
+        self.vocab = Vocab(annotations_file)
+        self.coco_caps = COCO(os.path.join(annotations_file))
+        self.image_folder = image_folder
+        self.batch_size = batch_size
+
+        '''
+        Get the length of all the captions available in the set. Needed for training
+        the model and to get only images with captions of a fixed length
+        '''
+        all_tokens = []
+        for index in tqdm(np.arange(len(self.index))):
+            test_id = self.coco.anns[self.index[index]]['image_id']
+            captions = load_captions(test_id, self.coco_caps)[0]['caption']
+            all_tokens.append(nltk.tokenize.word_tokenize(str(captions).lower()))
+
+        self.caption_lengths = [len(token) for token in all_tokens]
 
     def __len__(self):
         return len(self.index)
 
     def __getitem__(self, id):
-        # image = coco.loadImgs(self.index[id])
-        img_id = train_coco.anns[self.index[id]]['image_id']
-        img = train_coco.loadImgs(img_id)[0]
-        image = Image.open(os.path.join(train_file_path, img['file_name'])).convert('RGB')
+        # get the id of the image located in self.index and preprocess it
+        img_id = self.coco.anns[self.index[id]]['image_id']
+        img = self.coco.loadImgs(img_id)[0]
+        image = Image.open(os.path.join(self.image_folder, img['file_name'])).convert('RGB')
         image = self.transform(image)
-        # test = self.transform(io.imread(os.path.join(train_file_path, img['file_name'])))
-        print(load_captions(img_id))
-        return np.transpose(image)
+
+        # get the captions associated with the image id
+        captions = load_captions(img_id, self.coco_caps)[0]['caption']
+        caption_arr = []
+    
+        # tokenize and vectorize the captions
+        tokens = nltk.tokenize.word_tokenize(str(captions).lower())
+        caption_arr.append(self.vocab(self.vocab.start_word))
+        caption_arr.extend([self.vocab(token) for token in tokens])
+        caption_arr.append(self.vocab(self.vocab.end_word))
+            
+        caption_arr = torch.Tensor(caption_arr).long()
+
+        return image, caption_arr
+
+    def get_indices(self):
+        '''
+        This function returns a subset of the dataset of size batch_size where the length of the captions are equal to each other
+        This is necessary because the caption tensors need to be of the same length
+        '''
+        sel_length = np.random.choice(self.caption_lengths)
+        all_indicies = np.where([self.caption_lengths[i] == sel_length for i in np.arange(len(self.caption_lengths))])[0]
+        indicies = list(np.random.choice(all_indicies, size=self.batch_size))
+        return indicies
+    
 
     # Define a transform to pre-process the training images.
     transform = transforms.Compose([ 
-        # transforms.ToPILImage(),
         transforms.Resize(256),                          # smaller edge of image resized to 256
         transforms.RandomCrop(224),                      # get 224x224 crop from random location
         transforms.RandomHorizontalFlip(),               # horizontally flip image with probability=0.5
@@ -67,30 +112,127 @@ class transform_dataset(data.Dataset):
         transforms.Normalize((0.485, 0.456, 0.406),      # normalize image for pre-trained model
                             (0.229, 0.224, 0.225))])
 
-dataset = transform_dataset(indices=ids) 
-# Set the minimum word count threshold.
-vocab_threshold = 5
- 
-# Specify the batch size.
-batch_size = 10
+# hyperparameters
+BATCH_SIZE = 32
+VOCAB_THRESHOLD = 5
+EPOCHS = 10
+EMBED_SIZE = 256
+HIDDEN_SIZE = 512
+
+train_dataset = transform_dataset(coco=train_coco, annotations_file=os.path.join('./cocoapi/annotations','captions_train2014.json'), image_folder=train_file_path, batch_size=BATCH_SIZE) 
+val_dataset = transform_dataset(coco=val_coco, annotations_file=os.path.join('./cocoapi/annotations', 'captions_val2014.json'), image_folder=val_file_path, batch_size=BATCH_SIZE)
+
  
 # Load the data
-data_loader = torch.utils.data.DataLoader(dataset, 
-                                        batch_size=batch_size,
-                                        shuffle=True)
+train_loader = torch.utils.data.DataLoader(train_dataset, 
+                                           batch_size=BATCH_SIZE,
+                                           shuffle=True,
+                                           num_workers=0)
+val_loader = torch.utils.data.DataLoader(train_dataset, 
+                                         batch_size=BATCH_SIZE,
+                                         shuffle=True,
+                                         num_workers=0)
 
 # visualize images
-count = 0
-for images in data_loader:
-    for image in images:
-        if count < 10:
-            plt.axis('off')
-            plt.imshow(image)
-            plt.show()
-        count += 1
+# count = 0
+# for images in data_loader:
+#     for image in images:
+#         if count < 10:
+#             plt.axis('off')
+#             plt.imshow(image)
+#             # print(caption)
+#             plt.show()
+#         count += 1
 
-# data_loader = torch.utils.data.DataLoader(dataset=transform_train,
-#                          mode='train',
-#                          batch_size=batch_size,
-#                          vocab_threshold=vocab_threshold,
-#                          vocab_from_file=False)
+# get the most frequent number of caption lengths and sort in reverse order (most frequent length is index 0)
+counter = Counter(train_loader.dataset.caption_lengths)
+lengths = sorted(counter.items(), key=lambda pair: pair[1], reverse=True)
+
+indices = train_loader.dataset.get_indices()
+print('{} sampled indices: {}'.format(len(indices), indices))
+new_sampler = data.sampler.SubsetRandomSampler(indices=indices)
+train_loader.batch_sampler.sampler = new_sampler
+vocab_size = len(train_loader.dataset.vocab)
+
+# for batch in train_loader:
+#     images, captions = batch
+#     break
+
+###############################################################################
+###############################################################################
+# TRAINING
+###############################################################################
+###############################################################################
+
+# training information
+encoder = EncoderCNN(EMBED_SIZE)
+decoder = DecoderRNN(EMBED_SIZE, HIDDEN_SIZE, vocab_size)
+loss_func = nn.CrossEntropyLoss()
+learnable_params = list(decoder.parameters()) + list(encoder.embed.parameters()) + list(encoder.bn.parameters())
+optimizer = torch.optim.Adam(params=params, lr=0.001)
+
+train_step = np.ceil(len(train_loader.dataset.caption_lengths) / train_loader.batch_sampler.batch_size)
+val_step = np.ceil(len(val_loader.dataset.caption_lengths) / val_loader.batch_sampler.batch_size)
+
+print('training steps:', train_step)
+print('validation steps:', val_step)
+
+if torch.cuda.is_available():
+    decoder = decoder.cuda()
+    encoder = encoder.cuda()
+    loss_func = nn.CrossEntropLoss().cuda()
+
+# losses
+train_loss = []
+val_loss = []
+val_bleu_scores = [] # BLEU - Bilingual Evaluation Understudy score - widely adopted, inexpensive evaluation of sentence. 1 is perfect match, 0 is perfect mismatch
+best_bleu_score = float('-INF')
+
+start_time = time.time()
+for epoch in range(0, EPOCHS):
+    t_loss = train(train_loader, 
+                   encoder, 
+                   decoder, 
+                   loss_func, 
+                   optimizer, 
+                   vocab_size, 
+                   epoch, 
+                   train_step)
+    v_loss, bleu_score = validate(val_loader, 
+                                  encoder, 
+                                  decoder, 
+                                  loss_func, 
+                                  train_loader.dataset.vocab, 
+                                  epoch, 
+                                  val_step)
+                                        
+    train_loss.append(t_loss)
+    val_loss.append(v_loss)
+    val_bleu_scores.append(bleu_score)
+
+    if bleu_score > best_bleu_score:
+        print('Validation Bleu score improved from {:0.4f} to {:0.4f}, saving model to best_model.pkl'.format(best_bleu_score, bleu_score))
+        best_bleu_score = bleu_score
+        fname = os.path.join('./models', 'best_model.pkl')
+        save_epoch(fname, encoder, decoder, optimizer, train_loss, val_loss, bleu_score, val_bleu_scores, epoch)
+    else:
+        print('saving model to model_{}.pkl'.format(epoch))
+        fname = os.path.join('./models', 'model_{}.pkl'.format(epoch))
+        save_epoch(fname, encoder, decoder, optimizer, train_loss, val_loss, bleu_score, val_bleu_scores, epoch)
+        print('Epoch [%d/%d] took %ds' % (epoch, EPOCHS, time.time() - start_time))
+    
+    if epoch > 5:
+        if early_stopping(val_bleu_scores, e):
+            break
+    start_time = time.time()
+
+
+
+
+features = encoder(images)
+
+
+if torch.cuda.is_available():
+    captions = captions.cuda()
+
+outputs = decoder(features, captions)
--- vocab.py
+++ vocab.py
@@ -1,15 +1,17 @@
 import nltk
-from pycocotools.coco
+import json
+import os
+from pycocotools.coco import COCO
 from collections import Counter
 
 class Vocab():
-    def __init__(self, vocab_threshold, annotations_file):
+    def __init__(self, annotations_file, vocab_threshold=5):
         self.vocab_threshold = vocab_threshold
         self.annotations_file = annotations_file
         self.start_word = '<start>'
         self.end_word = '<end>'
         self.unknown_word = '<unknown>'
-        self.build_vocab()
+        self.get_vocab()
 
     def __call__(self, word):
         if not word in self.word_to_index:
@@ -18,6 +20,26 @@ class Vocab():
     
     def __len__(self):
         return len(Self.word_to_index)
+
+    def get_vocab(self):
+        if os.path.exists('./built_vocab.json'):
+            temp = None
+            with open('./built_vocab.json', 'r') as f:
+                # string = f.read().split(';')
+                temp = json.load(f)
+            self.word_to_index = temp['word_to_index']
+            self.index_to_word = temp['index_to_word']
+        else:
+            self.build_vocab()
+            temp = {'word_to_index': self.word_to_index,
+                    'index_to_word': self.index_to_word}
+            # word_to_index_string = json.dumps(self.word_to_index)
+            # index_to_word_string = json.dumps(self.index_to_word)
+            # result = word_to_index_string + ";" + index_to_word_string
+            with open('./built_vocab.json', 'w') as f:
+                f.write(json.dumps(temp))
+
+            # self.word_to_index = temp[]
 
     def build_vocab(self):
         self.init_vocab()
